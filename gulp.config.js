c504bdfbf749cc685f6a2c0e606c31badd6ea6e1
egoroffvv <100635937+egoroffvv@users.noreply.github.com>
2022-06-05 19:45:58 +0300

    Add files via upload

---
+++ gulp.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  SRC_PATH : 'src',
+  DIST_PATH : 'dist',
+  STYLE_LIBS : [
+    'node_modules/normalize.css/normalize.css'
+  ],
+  JS_LIBS : [
+    'node_modules/jquery/dist/jquery.js',
+    'node_modules/bxslider/dist/jquery.bxslider.min.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.min.js',
+    'node_modules/jquery-touchswipe/jquery.touchSwipe.min.js',
+    'node_modules/mobile-detect/mobile-detect.min.js'
+  ]
+ }
